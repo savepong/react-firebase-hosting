3a329e523e07fdf43f40b7fac0c50dc9d1ebc956
Savepong <savepong.com@gmail.com>
2019-02-19 12:12:15 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            สวัสดี <code>React</code> from Firebase hosting.
           </p>
           <a
             className="App-link"
